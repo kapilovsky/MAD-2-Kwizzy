6bd87924bae64570d209752b2603520336b0b999
k4p1l <k4p1ll.23@gmail.com>
2025-01-28 14:29:19 +0530

    minor bug fix

--- Kwizzy/client/src/views/Admin/UserQuizDetails.vue
+++ Kwizzy/client/src/views/Admin/UserQuizDetails.vue
@@ -300,7 +300,7 @@ watch(
 
 const backToDashboard = async () => {
   try {
-    await router.push(`/admin/user`);
+    await router.push(`/admin/dashboard`);
     window.location.reload();
   } catch (error) {
     console.error("Navigation error:", error);
